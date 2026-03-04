5441756232220edd746e7acc549c4320e912c78a
Dante <g.beardicus@gmail.com>
2026-03-05 01:46:23 +0400

    Remove all React components and JavaScript bundles related to the WW3 game frontend.

--- entry.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WW3Game from './ww3-simulation.jsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(WW3Game));
